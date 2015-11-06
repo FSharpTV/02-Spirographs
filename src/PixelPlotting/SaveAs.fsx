053e24b1d3f92b6619733929472a52acf8d35dd0
Mark Gray <markgray.online@googlemail.com>
2015-11-06 15:07:02 +0000

    Adding code for the Limitations video

---
+++ src/PixelPlotting/SaveAs.fsx
@@ -0,0 +1,20 @@
+﻿#load "PixelPlotter.fs"
+
+// First
+#load "PixelPlotter.fs"
+
+// Second
+open PixelPlotting.Plotter // Open the module
+open System.Drawing // open so we can access the colors easily
+
+
+// Third
+// Manual Line draw
+bitmap.SetPixel(5,7, Color.Black)
+bitmap.SetPixel(6,7, Color.Black)
+bitmap.SetPixel(7,7, Color.Black)
+bitmap.SetPixel(8,7, Color.Black)
+bitmap.SetPixel(9,7, Color.Black)
+
+// Forth
+bitmap |> saveAs "simpleLine.png" // call our saver function passing in the bitmap we have manipulated
