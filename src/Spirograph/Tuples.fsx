d09cf1691e6b6df0aeb96b3594a7253f243a572a
Mark Gray <markgray.online@googlemail.com>
2015-11-11 19:09:53 +0000

    Added Tuples.fsx to the solution

---
+++ src/Spirograph/Tuples.fsx
@@ -0,0 +1,15 @@
+﻿let location = (77.51,166.40,21)
+
+let longAlt location =
+    let long,_,alt = location
+    (alt, long)
+
+printfn "%A" (snd (longAlt location))
+
+let birthday = 21,1,1970
+
+let reverse birth =
+    let dd,mm,yy = birth
+    yy,mm,dd
+
+printfn "%A" (reverse birthday)
