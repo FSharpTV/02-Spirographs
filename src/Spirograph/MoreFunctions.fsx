96873f09669cc8931c47df82a2ea2879502dde0c
Mark Gray <markgray.online@googlemail.com>
2015-11-11 21:15:51 +0000

    Adding script for the 'Playing with functions' video

---
+++ src/Spirograph/MoreFunctions.fsx
@@ -0,0 +1,27 @@
+﻿// Bryony's children 
+
+//  bob     = 2
+//  sam     = 4
+//  jon     = 7
+//  sarah   = 8
+
+let childAges = (2,4,7,8)
+
+let addChildAges ages =
+    let bob, sam, jon, sarah = ages
+    bob + sam + jon + sarah
+
+let roomAge bob =
+    let addBobsAge sam =
+        let addSamsAge jon =
+            let addJonsAge sarah =
+                bob + sam + jon + sarah
+            addJonsAge
+        addSamsAge
+    addBobsAge
+
+let addAges bob sam jon sarah =
+    bob + sam + jon + sarah
+
+let triArea height width =
+    (height * width) / 2
