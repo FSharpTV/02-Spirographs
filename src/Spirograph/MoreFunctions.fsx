45242e618bff206345e55bcdcdaa5eeba02f3681
Mark Gray <markgray.online@googlemail.com>
2015-11-12 20:12:33 +0000

    Updating the MoreFunctions.fsx file to be simpler

--- src/Spirograph/MoreFunctions.fsx
+++ src/Spirograph/MoreFunctions.fsx
@@ -1,27 +1,18 @@
-﻿// Bryony's children 
+﻿// bob, sam
 
-//  bob     = 2
-//  sam     = 4
-//  jon     = 7
-//  sarah   = 8
-
-let childAges = (2,4,7,8)
+let childAges = 2,4
 
 let addChildAges ages =
-    let bob, sam, jon, sarah = ages
-    bob + sam + jon + sarah
+    let bob, sam = ages
+    bob + sam
 
-let roomAge bob =
-    let addBobsAge sam =
-        let addSamsAge jon =
-            let addJonsAge sarah =
-                bob + sam + jon + sarah
-            addJonsAge
-        addSamsAge
-    addBobsAge
+let addAges bob sam =
+    bob + sam
 
-let addAges bob sam jon sarah =
-    bob + sam + jon + sarah
+let fnChildAges bob =
+    let addSamsAgeToBobs sam =
+        bob + sam
+    addSamsAgeToBobs
 
 let triArea height width =
-    (height * width) / 2
+    (height*width)/2
