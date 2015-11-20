ed32532cb76b5eedb798de7a9b914adeb3b265ba
Mark Gray <markgray.online@googlemail.com>
2015-11-20 11:30:06 +0000

    Adding the files for the 'Chaining function nicely' video

---
+++ src/Spirograph/Pipes.fsx
@@ -0,0 +1,16 @@
+﻿let square num = num * num
+let divide num = num / 3
+let adder  x y = x + y
+
+let brackets =
+    (adder 6
+        (square 
+            (divide 
+                (square 6))))
+
+let piped =
+    6 
+    |> square 
+    |> divide 
+    |> square
+    |> adder 6
