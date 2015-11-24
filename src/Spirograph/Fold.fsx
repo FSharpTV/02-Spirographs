5d6d8d2efc7fea44a39ac1672012e831ba907448
Mark Gray <markgray.online@googlemail.com>
2015-11-24 18:19:41 +0000

    Added fsx for Fold video

--- src/Spirograph/Fold.fsx
+++ src/Spirograph/Fold.fsx
@@ -1,1 +1,8 @@
-﻿
+﻿let timeSpent = [30;83;49;12;74;74;86;82;15;43;24;87;3;49;81]
+
+List.fold (fun s hrs -> s + hrs) 0 timeSpent
+
+let populations = [81276000;67063000;65081000]
+
+List.fold (fun s pop -> s + pop) 0 populations
+
